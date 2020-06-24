ee96f65598829ef10cb799416d74517f6cdcf3af
Fabio Muller <fabioml10@yahoo.com.br>
2020-06-24 10:02:20 -0300

    Finished

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from "@angular/core";
 import { Navbar } from './component/navbar/navbar.component';
 import { RouterModule } from '@angular/router';
+import { Error404Component } from './component/404/404.component';
 
 @NgModule({
   declarations: [
-    Navbar
+    Navbar,
+    Error404Component
   ],
   imports: [
-    RouterModule
+    RouterModule.forChild([
+      {
+        path: '**', component: Error404Component
+      }
+    ])
   ],
   exports: [
     Navbar
